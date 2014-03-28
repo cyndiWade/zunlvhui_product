47928325887798c7ce904d55ea96697354db7e44
cyndiWade <zhanglin492103904@qq.com>
2014-03-28 14:23:15 +0800

    wade

--- App/Lib/Action/Hotel/PropertyAction.class.php
+++
@@ -1,9 +0,0 @@
-<?php
-class PropertyAction extends HotelBaseAction{
-	
-	public function index(){
-	    $url = $_SERVER["REQUEST_URI"];
-	    $this->assign('hover',parent::getAction($url));
-		$this->display();
-	}
-}
--- App/Public/media/plugins/fullcalendar-1.6.4/readme.txt
+++
@@ -1,3 +0,0 @@
-详细的参数以及说明文档都在
-
-external-dragging_demo.html  文件中
