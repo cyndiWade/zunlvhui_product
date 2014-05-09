791a96e1b55c586333bc6755b8a4c663271e67e4
972980756@qq.com <972980756@qq.com>
2014-05-09 11:37:17 +0800

    tjr 11.08

--- App/Lib/Action/Api/WeixinAction.class.php
+++ App/Lib/Action/Api/WeixinAction.class.php
@@ -697,8 +697,8 @@ private function receiveText($object)
 	      $data = array(
 					    	'user_code'=>"$user_code",
 					    	'hotel_add'=>"$text",
-				    	    'hotel_name'=>$arr_item['hotel_name'],
-				    	    'hotel_id'=>$arr_item['hotel_id'],
+				    	    'hotel_name'=>$arr['hotel_name'],
+				    	    'hotel_id'=>$arr['hotel_id'],
 					    	'step'=>$step+2,
 					    	'endtime'=>time()+$T
 				    	); 
