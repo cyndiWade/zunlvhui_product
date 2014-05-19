4aeb67642a9e5c344be4e792586ddf91a6d3ea2b
972980756@qq.com <972980756@qq.com>
2014-05-19 14:19:18 +0800

    tjr 14.19

--- App/Lib/Model/Admin/WxUserModel.class.php
+++ App/Lib/Model/Admin/WxUserModel.class.php
@@ -5,12 +5,14 @@ class  WxUserModel extends  AdminBaseModel {
 
 		//获取所有订单列表
 		public function seek_all_data ($field='*')  {
-			$hotel_remarks  = $this->get_code_exp('code_id,hotel_remarks');
-		    $data = $this->field($field)->select();
+			$remarks  = $this->get_code_exp('code_id,hotel_remarks,yuangong');
+		    
+			$data = $this->field($field)->select();
 		    $USERFROM = C('USERFROM');
 		    $SUBSCRIBE = C('SUBSCRIBE');
 			foreach($data as $k=>$v){
-				$data[$k]['hotel_remarks']=$hotel_remarks[$v['code_id']];
+				$data[$k]['hotel_remarks']=$remarks['hotel_remarks'][$v['code_id']];
+				$data[$k]['yuangong']     =$remarks['yuangong'][$v['code_id']];
 				$data[$k]['userfrom']=$USERFROM[$v['is_from']]['explain'];
 				$data[$k]['subscribe']=$SUBSCRIBE[$v['subscribe']]['explain'];
 			}
@@ -29,11 +31,14 @@ class  WxUserModel extends  AdminBaseModel {
 			$data = $this->field($field)
 			->table($this->prefix.'wx_code')
 			->where($where)->select();
-			$arr = array();
+			$hotel_remarks = array();
+			$yuangong = array();
 			foreach ($data as $k=>$v){
-				$arr[$v['code_id']]= $v['hotel_remarks'];
+				$hotel_remarks[$v['code_id']]= $v['hotel_remarks'];
+				$yuangong[$v['code_id']]=$v['yuangong'];
 			}
-			return $arr;
+			return array('hotel_remarks'=>$hotel_remarks,'yuangong'=>$yuangong);
+			
 		}
 }
 
