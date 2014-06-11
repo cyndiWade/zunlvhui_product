6bb5643a7a7e49852af2f8e41b8cb2d3446db899
972980756@qq.com <972980756@qq.com>
2014-06-11 18:08:16 +0800

    tjr 10.29

--- App/Lib/Action/Api/WeixinAction.class.php
+++ App/Lib/Action/Api/WeixinAction.class.php
@@ -280,7 +280,8 @@ class WeixinAction extends AppBaseAction{
 					        if(count($arr_item)>0){
 						       $resultStr = $this->transmitNews($object, $arr_item, $flag = 0);
 							}else{
-                                $contentStr ="没有预定送免房的优惠";
+                                //$contentStr ="没有预定送免房的优惠";
+                                $contentStr ="正在开发中，敬请期待";
 						        $resultStr = $this->transmitText($object, $contentStr);
 							}
 							break;
